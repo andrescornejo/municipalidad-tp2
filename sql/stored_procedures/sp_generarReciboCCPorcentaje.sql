5a728e17bc43c0bb920878b36d217030324cfa77
palpizarm <pabloalpizar99@gmail.com>
2020-07-10 12:06:50 -0600

    add sp to generate bill for porcentaje

--- sql/stored_procedures/sp_generarReciboCCPorcentaje.sql
+++ sql/stored_procedures/sp_generarReciboCCPorcentaje.sql
@@ -13,32 +13,74 @@ as
 begin
 	begin try
 		set nocount on
+		DECLARE @Monto MONEY
+		DECLARE @Porcentaje FLOAT
+		DECLARE @idPropiedad INT
+		DECLARE @QDias INT
+
 		DECLARE @DiaCobro INT = (SELECT C.DiaEmisionRecibo 
                             FROM [dbo].[ConceptoCobro] C 
                             WHERE C.id = @inIdCC)
 
-        IF @DiaCobro != (SELECT EXTRACT (DAY FROM @inFecha))
+        IF @DiaCobro != (SELECT DAY(@inFecha))
         BEGIN
             RETURN
         END
 
 		DECLARE @tmpPropiedadesTipoCC TABLE(
-            idPropiedad INT
+            idPropiedad INT,
+			valor MONEY
         )
 
 		DECLARE @tmpRecibos TABLE (
-			
+			idPropiedad INT,
+            idConceptoCobro INT,
+            Monto MONEY
 		)
 
-		INSERT INTO @tmpPropiedadesTipoCC (idPropiedad)
-		SELECT CP.idPropiedad FROM [dbo].[CCenPropiedad] CP 
+		SET @Porcentaje = (SELECT CC.ValorPorcentaje FROM [dbo].[CC_Porcentaje] CC WHERE CC.id = @inIdCC)
+		SET @QDias = (SELECT C.QDiasVencimiento FROM [dbo].[ConceptoCobro] C WHERE C.id = @inIdCC)
+
+		INSERT INTO @tmpPropiedadesTipoCC (idPropiedad, valor)
+		SELECT CP.idPropiedad,  P.Valor FROM [dbo].[CCenPropiedad] CP
+		INNER JOIN [dbo].[Propiedad] P ON CP.idPropiedad = P.id
 		WHERE CP.idConceptoCobro = @inIdCC
 
-		INSERT INTO [dbo].[Recibo] (
 
-		)
+		WHILE (SELECT COUNT(*) FROM @tmpPropiedadesTipoCC) > 0
+		BEGIN
+			-- seleccionamos la primera propiedad
+			SELECT TOP 1 @idPropiedad = tmp.idPropiedad FROM @tmpPropiedadesTipoCC tmp
+			SET @Monto = ((SELECT TOP 1 tmp.valor FROM @tmpPropiedadesTipoCC tmp) * (@Porcentaje / 100))
+			-- Quitamos esta propiedad de la tabla
+			DELETE @tmpPropiedadesTipoCC WHERE @idPropiedad = idPropiedad
 
+			INSERT INTO @tmpRecibos (idPropiedad, idConceptoCobro, Monto)
+			SELECT @idPropiedad, @inIdCC, @Monto
+		END
 
+		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+        BEGIN TRANSACTION
+
+			INSERT INTO [dbo].[Recibo](
+				idPropiedad,
+				idConceptoCobro,
+				fecha,
+				fechaVencimiento,
+				monto,
+				esPendiente,
+				activo)
+			SELECT 
+				tmp.idPropiedad,
+				tmp.idConceptoCobro,
+				@inFecha,
+				DATEADD(DAY,@QDias,@inFecha),
+				tmp.Monto,
+				1,
+				1
+			FROM @tmpRecibos tmp
+		
+		COMMIT
 	end try
 	begin catch
 		IF @@TRANCOUNT > 0
